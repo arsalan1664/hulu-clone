18a9e972fa9be1400dff7914d4845a60b927d103
Arsalan Ali <arsalan1664@gmail.com>
2023-07-13 21:19:21 +0000

    Header complete

--- my-app/app/component/Header.js
+++ my-app/app/component/Header.js
@@ -1,9 +1,18 @@
+import { HomeIcon, BoltIcon, UserIcon, CheckBadgeIcon, RectangleStackIcon, MagnifyingGlassIcon} from '@heroicons/react/24/outline'
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 
 function Header(){
     return(
-        <header className=''>
-            <h1>header</h1>
+        <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+            <div className='flex flex-grow justify-evenly max-w-2xl'>
+                <HeaderItem Icon={HomeIcon} title='HOME'/>
+                <HeaderItem Icon={BoltIcon} title='TRENDING'/>
+                <HeaderItem Icon={CheckBadgeIcon} title='VERIFIED'/>
+                <HeaderItem Icon={RectangleStackIcon} title='COLLECTIONS'/>
+                <HeaderItem Icon={MagnifyingGlassIcon} title='SEARCH'/>
+                <HeaderItem Icon={UserIcon} title='USER'/>
+            </div>
             <Image className='object-contain' 
              src="https://links.papareact.com/ua6"
              width={200}
