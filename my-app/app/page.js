661bbd89a1246716449fa3ee8df98a3a9a267ce1
Arsalan Ali <arsalan1664@gmail.com>
2023-07-15 19:22:02 +0000

    Nav

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,11 +1,11 @@
-
+import Nav from './component/Nav'
 import Header from './component/Header'
 
 export default function Home() {
   return (
     <div>
       <Header/>
-      {/* Nav */}
+      <Nav/>
       {/* Results */}
     </div>
   )
